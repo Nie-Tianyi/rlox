c2a7d3bedfc90c12975d45b557dab4b17d932cb5
nty-mac <1355705558@qq..com>
2024-12-12 01:59:53 +0800

    init repo

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+mod reporter;
+mod token;
+mod scanner;
+
+use clap::{Parser, ValueHint};
+use std::io::{Read, Write};
+use std::path::Path;
+use std::{fs, io};
+
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(value_parser = clap::value_parser!(String),num_args = 1,
+        value_hint = ValueHint::FilePath,)]
+    path: Option<String>,
+}
+
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.path {
+        Some(path) => run_file(path),
+        None => run_prompt(),
+    }
+}
+
+fn run_prompt() {
+    let stdin = io::stdin();
+    let mut input = String::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("fail to flush");
+
+        stdin.read_line(&mut input).expect("fail to read from terminal");
+        if input == "\n" { break; }
+
+        run(input.clone());
+        input.clear();
+    }
+}
+
+fn run_file(path: impl AsRef<Path>) {
+    let mut file = fs::File::open(path).expect("fail to find given file");
+    let mut content = String::new();
+
+    file.read_to_string(&mut content).expect("fail to read given file");
+
+    run(content);
+}
+
+
+fn run(source_code: String) {
+    println!("run {:?}", source_code.into_bytes())
+}
